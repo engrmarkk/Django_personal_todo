b726e25df529f001d56006a8c6412d7d88c59c89
Adeniyi Olanrewaju <adeniyiboladale@yahoo.com>
2023-04-29 08:56:25 +0100

    register the logout url

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.login, name='login'),
     path('todo', views.index, name='index'),
     path('delete/<todo_id>/', views.delete_todo, name='delete_todo'),
-    path('complete/<todo_id>/', views.complete_todo, name='complete_todo')
+    path('complete/<todo_id>/', views.complete_todo, name='complete_todo'),
+    path('logout', views.logout, name='logout'),
 ]
